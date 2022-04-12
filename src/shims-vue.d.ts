d74864330b303b70720cf924f07eac1d04b9cd1e
LovlyBoi <728881636@qq.com>
2022-04-12 19:45:52 +0800

    feat: vue-toaster类型声明

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -6,4 +6,30 @@ declare module '*.vue' {
 }
 
 declare module 'masonry-layout'
-declare module '@meforma/vue-toaster'
+
+declare module '@meforma/vue-toaster' {
+  type Toast = {
+    show: (msg: string, options?: Options) => void
+    success: (msg: string, options?: Options) => void
+    warning: (msg: string, options?: Options) => void
+    error: (msg: string, options?: Options) => void
+    info: (msg: string, options?: Options) => void
+  }
+  type Options = {
+    position:
+      | 'top'
+      | 'bottom'
+      | 'top-right'
+      | 'bottom-right'
+      | 'top-left'
+      | 'bottom-left'
+    duration: number
+    dismissible?: boolean
+    maxToasts?: number | false
+    pauseOnHover?: boolean
+  }
+
+  const createToaster: (options: Options) => Toast
+
+  export { install, createToaster }
+}
--- src/views/PublishView.vue
+++ src/views/PublishView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="publish-view white-card-shadow">
-    <el-form :model="form" label-width="120px">
-      <el-form-item label="标题">
+    <el-form :model="form" label-width="120px" :rules="formRules" ref="formRef">
+      <el-form-item label="标题" prop="title">
         <el-input style="width: 430px" v-model="form.title" />
       </el-form-item>
-      <el-form-item label="作者">
+      <el-form-item label="作者" prop="author">
         <el-input maxlength="20" style="width: 186px" v-model="form.author" />
       </el-form-item>
-      <el-form-item label="标签名">
+      <el-form-item label="标签名" prop="tagName">
         <el-input maxlength="20" style="width: 186px" v-model="form.tag.name" />
       </el-form-item>
-      <el-form-item label="标签颜色">
+      <el-form-item label="标签颜色" prop="tagColor">
         <el-select v-model="form.tag.color" placeholder=" ">
           <el-option
             v-for="item in tagColor"
@@ -20,7 +20,7 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="文章类型">
+      <el-form-item label="文章类型" prop="type">
         <el-select v-model="form.type" placeholder=" ">
           <el-option label="生活随笔" :value="1" />
           <el-option label="笔记" :value="2" />
@@ -48,16 +48,19 @@
         </el-upload>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">提交</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="handleSubmit(formRef)"
+          >提交</el-button
+        >
+        <el-button @click="handleFoolish">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import { UploadFilled } from '@element-plus/icons-vue'
+import type { FormInstance } from 'element-plus'
 import toast from '@/utils/toast'
 import type { Responce } from '@/network'
 
@@ -68,16 +71,33 @@ const tagColor = [
   { value: 'orange', label: '橙色' },
 ]
 
+const formRef = ref<FormInstance>()
+
 const form = reactive({
   title: '',
   author: '',
   tag: {
     name: '',
-    color: '',
+    color: 'yellow',
   },
   type: 1,
   filePath: '',
 })
+
+const formRules = {
+  title: [
+    { required: true, message: '标题还是需要的哦', trigger: 'blur' },
+    { min: 2, max: 25, message: '标题限制在2~25个字符呐', trigger: 'blur' },
+  ],
+  author: [
+    { required: true, message: '作者名也是必须的哦', trigger: 'blur' },
+    { min: 2, max: 10, message: '作者名限制在2~10个字符呐', trigger: 'blur' },
+  ],
+  tagName: [
+    { required: true, message: '填上一个4字标签名吧', trigger: 'blur' },
+    { min: 4, max: 4, message: '标签名要4个字哦', trigger: 'blur' },
+  ],
+}
 
 const file = reactive([])
 
@@ -87,6 +107,23 @@ const handleUploadSuccess = ({ code, data, msg }: Responce) => {
   } else {
     toast.warning(msg)
   }
+}
+
+const handleSubmit = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      if (!form.filePath) {
+        toast.success('先上传文章吧~')
+      }
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
+const handleFoolish = () => {
+  toast.success('这是一个没有啥用的按钮，但我就是想放在这')
 }
 </script>
 
